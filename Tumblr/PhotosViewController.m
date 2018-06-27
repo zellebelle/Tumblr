3d3a637f72273659347563d0b425ed8bb8258278
Gisele Phalo <zellebelle@fb.com>
2018-06-27 14:27:30 -0700

    add controllers

---
+++ Tumblr/PhotosViewController.m
@@ -0,0 +1,36 @@
+//
+//  PhotosViewController.m
+//  
+//
+//  Created by Gisele Phalo on 6/27/18.
+//
+
+#import "PhotosViewController.h"
+
+@interface PhotosViewController ()
+
+@end
+
+@implementation PhotosViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
